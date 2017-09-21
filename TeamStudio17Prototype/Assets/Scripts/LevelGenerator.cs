d250bc230d4db2e977d0e3508418bfe958da4ddf
Grant Wang <yidegrant@gmail.com>
2017-09-21 08:53:15 -0400

    Initialized folders and began creating level generator

---
+++ TeamStudio17Prototype/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+
+    public int mapHalfWidth;
+    public int mapHalfHeight;
+
+    public Transform[] blockTypes;
+    List<Vector2> critPath = new List<Vector2>();
+
+    public Vector2[] playerStartPos;
+
+	// Use this for initialization
+	void Start () {
+        playerStartPos = new Vector2[] {
+            new Vector2(-mapHalfWidth, mapHalfHeight),
+            new Vector2(mapHalfWidth, mapHalfHeight),
+            new Vector2(-mapHalfWidth, -mapHalfHeight),
+            new Vector2(mapHalfWidth, -mapHalfHeight),
+        };
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    class Vertex
+    {
+        public Vector2 location;
+        public Vertex parent;
+    }
+
+    void generateCritPath(Vector2 start)
+    {
+        Stack<Vertex> toBeSearched = new Stack<Vertex>();
+        Vertex startVertex = new Vertex();
+        startVertex.location = start;
+        toBeSearched.Push(startVertex);
+    }
+}
